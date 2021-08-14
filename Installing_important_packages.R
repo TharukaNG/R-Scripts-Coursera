e6238a1cc80f9c82642dd6bce4b808b3198bf3b4
Tharuka Gunasekara <66100720+TharukaNG@users.noreply.github.com>
2021-08-14 14:09:57 +0530

    Added files of exporting data as pdf in R-Studio

---
+++ Installing_important_packages.R
@@ -0,0 +1,16 @@
+install.packages("rmarkdown")
+install.packages("latex")
+install.packages("miktex")
+
+install.packages("rticles")
+install.packages("vitae")
+
+install.packages("pdflatex")
+
+install.packages('tinytex')
+tinytex::install_tinytex()
+
+
+
+
+
